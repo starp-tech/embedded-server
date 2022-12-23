69ac6b3d25ec8f172d373ccb58f20682322c2e73
Daniel <fiend.of.sound@gmail.com>
2022-12-23 04:44:00 +0400

    feat: stable and tested

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,24 @@ import networkAddress from 'network-address'
 
 args
   .option('media', 'a magnet link to media')
+  .option('binary', 'a binary of media file to stream')
   .option('seed', 'a magnet link to media for seed')
   .option('debug', 'extensive logs')
   .option('path', 'filesystem path to new media')
   .option('downloadLimit', "speed limit for media")
+  .option('hybrid', "enable webrtc hybrid mode")
 
 const flags = args.parse(process.argv)
 
 if(flags.debug) {
-	console.info("debugMode", flags.debug)
+	process.stdout.write("debugMode")
 	debugMode = true
 }
 else {
 	console = {
 		...console,
 		log:()=>{},
+		info:()=>{},
 		warn:()=>{},
 		error:()=>{}
 	}
@@ -35,8 +38,7 @@ let client:any;
 export const startServer = async (media:any, print?:boolean) => 
 	new Promise((resolve,reject)=>{
 		try {
-			if(debugMode)
-				console.info('startServer for media', media.magnetURI)
+			console.log('startServer for media', media.magnetURI)
 
 		  if (server) return
 
@@ -44,15 +46,23 @@ export const startServer = async (media:any, print?:boolean) =>
 		  server.listen(0, () => {
 		    const port = server.address().port
 		    const urlSuffix = ':' + port
+			  
+
 		    const info = {
-		      torrentKey: media.key,
+		    	files:media.files.map(f=>f.name),
+		    	port,
 		      localURL: 'http://localhost' + urlSuffix,
 		      networkURL: 'http://' + networkAddress() + urlSuffix,
 		      networkAddress: networkAddress()
 		    }
-		    
-		    if(debugMode || print)
-		    	console.info(info)
+		    const json = 
+		    		JSON.stringify(info)
+		    if(debugMode) {
+			    console.log(json)
+		    }
+		    if(print) {
+		    	process.stdout.write(json)
+		    }
 
 		    resolve(info)
 		  })
@@ -67,28 +77,28 @@ export const createMedia = async (filePath:string) => {
 	try {
 		client.seed(filePath, async (media:any) => {
     	// await startServer(media)
-			
-			if(debugMode)
-	    	console.info(media.torrentFile)
+    	console.log(media.torrentFile)
 
     	const mediaFile = media.torrentFile.toString("base64")
-    	console.info(mediaFile)
+    	process.stdout.write(mediaFile)
+    	// console.info(mediaFile)
 		})
 	} catch(err){
   	console.error("createMedia error", err)
 	}
 }
 
-export const addMedia = async (media) => {
+export const addMedia = async (media, isBuffer = false) => {
 
 	if(media.search("magnet") === -1) {
-		if(debugMode)
-			console.info('no magnet', media)
+		console.log('no magnet', media)
+		media = Buffer.from(media, 'base64')
 
-		media = Buffer.from(media, 'base64');
+		if(!isBuffer) {
+			media = media.toString("utf8");
+		}
 
-		if(debugMode)
-			console.info('no magnet', media)
+		console.log('no magnet', media)
 	}
 
 	client.add(
@@ -97,39 +107,52 @@ export const addMedia = async (media) => {
 	)
 }
 
+export const addMediaBuffer = async (media) => addMedia(media, true)
+
 const start = () => {
-	client = new WebTorrent.default({
-		downloadLimit:flags.downloadLimit
-	})
-	if(debugMode)
-		console.info('Start With Flags', flags)
+	try {
+		console.info('start client')
+		client = new WebTorrent.default({
+			downloadLimit:flags.downloadLimit
+		})
 
-	if(flags.media) {
-		if(debugMode)
-			console.info("client add media")
-		addMedia(flags.media)
+		console.log('did start client', client)
+
+		if(flags.media) {
+			console.log("client add media")
+			addMedia(flags.media)
+		}
+
+		if(flags.binary) {
+			console.log("client add binary")
+			addMediaBuffer(flags.binary)
+		}
+
+		if(flags.path) 
+			createMedia(flags.path)
+
+		console.log("Wait For App")
+
+	} catch(err) {
+		console.error('app start error', err)
 	}
-
-	if(flags.path) 
-		createMedia(flags.path)
-
-	if(debugMode)
-		console.info("Wait For App")
 }
 
 export const setup = async () => {
-	try {
-		const obj = await import('webtorrent-hybrid')
-		WebTorrent = obj
-		if(debugMode)
-			console.info('import hybrid', WebTorrent)
-	} catch(err) {
-		console.error("import hybrid error", err)
+	console.info('Start With Flags', flags)
+	console.info('start setup hybrid')
+	if(flags.hybrid) {
+		try {
+			const obj = await import('webtorrent-hybrid')
+			WebTorrent = obj
+			console.log('import hybrid', WebTorrent)
+		} catch(err) {
+			console.error("import hybrid error", err)
+		}
 	}
+
+	console.log('after setup hybrid')
 	await start()
 }
-
-
-export default setup
 
 setup()
